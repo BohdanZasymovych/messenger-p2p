56b8c7658e7e2cda985ea56277601796d4205024
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2025-04-01 17:20:28 +0300

    Indefinite connecting state of peers fixed

--- messenger.py
+++ messenger.py
@@ -21,15 +21,43 @@ ICE_CONFIG = RTCConfiguration(
 
 SERVER_URL = "ws://localhost:8000"  # URL WebSocket сервера
 
-def set_data_channel_events(data_channel: RTCDataChannel, open_event: asyncio.Event = None):
-    """Обробник подій для data channel"""
+def set_peer_connection_events(peer_connection: RTCPeerConnection):
+    """Set events behavior for data channel"""
 
-    data_channel.on('error', lambda error: print("Data channel error:", error))
+    @peer_connection.on("connectionstatechange")
+    def on_connection_state_change():
+        logging.info(f"Connection state changed: {peer_connection.connectionState}")
+        print(f"Connection state changed: {peer_connection.connectionState}")
+
+    @peer_connection.on("iceconnectionstatechange")
+    def on_ice_state_change():
+        logging.info(f"Ice connection state changed: {peer_connection.iceConnectionState}")
+        print(f"Ice connection state changed: {peer_connection.iceConnectionState}")
+
+    @peer_connection.on("icegatheringstatechange")
+    def on_ice_gathering_change():
+        logging.info(f"Ice gathering state changed: {peer_connection.iceGatheringState}")
+        print(f"Ice gathering state changed: {peer_connection.iceGatheringState}")
+
+    @peer_connection.on("signalingstatechange")
+    def on_signaling_state_change():
+        logging.info(f"Signaling state changed: {peer_connection.signalingState}")
+        print(f"Signaling state changed: {peer_connection.signalingState}")
+
+
+def set_data_channel_events(data_channel: RTCDataChannel, open_event: asyncio.Event=None) -> None:
+    """Set events behavior for data channel"""
+
+    @data_channel.on("error")
+    def on_error(error):
+        print("Data channel error:", error)
+        logging.error(f"Data channel error: {error}")
 
     @data_channel.on('message')
     def on_message(message):
         print(f'Peer ({datetime.now().strftime("%H:%M:%S")}): {message}')
         logging.debug(f'Message received: {message}')
+        # asyncio.create_task(print_stats(peer_connection))
 
     @data_channel.on('open')
     def on_open():
@@ -40,8 +68,9 @@ def set_data_channel_events(data_channel: RTCDataChannel, open_event: asyncio.Ev
     @data_channel.on('close')
     def on_close():
         print('\nData channel was closed')
-        logging.warning('Exiting program, data channel closed')
+        logging.warning('Exiting program, data channel was closed')
         sys.exit(1)
+
 
 async def message_loop(data_channel: RTCDataChannel):
     """Асинхронний цикл обміну повідомленнями"""
@@ -52,34 +81,45 @@ async def message_loop(data_channel: RTCDataChannel):
             logging.debug(f'Message sent: {message}')
             data_channel.send(message)
 
+
 async def run_offer():
     """Функція для `offerer`"""
     data_channel_open_event = asyncio.Event()
+
+    # Create peer connection
     pc = RTCPeerConnection(configuration=ICE_CONFIG)
 
+    # Set behavior for connection state change
+    set_peer_connection_events(pc)
+
+    # Create data channel and set its behavior on events
     dc = pc.createDataChannel('channel')
     set_data_channel_events(dc, data_channel_open_event)
 
-    @pc.on("connectionstatechange")
-    def on_connection_state_change():
-        print(f"Connection state changed: {pc.connectionState}")
 
+    # Create and set offer
+    offer = await pc.createOffer()
+    await asyncio.sleep(5)
+    await pc.setLocalDescription(offer)
+
+    logging.info(
+        f"Offer created: {dict({'sdp': pc.localDescription.sdp,'type': pc.localDescription.type})}"
+    )
 
     async with websockets.connect(SERVER_URL) as websocket:
         await websocket.send(json.dumps({"type": "register", "user_id": "offerer"}))
 
-        offer = await pc.createOffer()
-        logging.info(offer)
-        await pc.setLocalDescription(offer)
 
-        await websocket.send(json.dumps({"type": "offer", "sdp": offer.sdp, "user_id": "offerer"}))
+        await websocket.send(json.dumps({"type": "offer", "sdp": pc.localDescription.sdp, "user_id": "offerer"}))
 
         response = await websocket.recv()
+        print(f'Response received: {response}')
         data = json.loads(response)
 
         if data["type"] == "answer":
             answer = RTCSessionDescription(sdp=data["sdp"], type="answer")
             await pc.setRemoteDescription(answer)
+            print("remote description set")
 
     await data_channel_open_event.wait()
     try:
@@ -103,9 +143,25 @@ async def run_answer():
         logging.info(f"Connection state changed: {pc.connectionState}")
         print(f"Connection state changed: {pc.connectionState}")
 
+    @pc.on("iceconnectionstatechange")
+    def on_ice_state_change():
+        logging.info(f"Ice connection state changed: {pc.iceConnectionState}")
+        print(f"Ice connection state changed: {pc.iceConnectionState}")
+
+    @pc.on("icegatheringstatechange")
+    def on_ice_gathering_change():
+        logging.info(f"Ice gathering state changed: {pc.iceGatheringState}")
+        print(f"Ice gathering state changed: {pc.iceGatheringState}")
+
+    @pc.on("signalingstatechange")
+    def on_signaling_state_change():
+        logging.info(f"Signaling state changed: {pc.signalingState}")
+        print(f"Signaling state changed: {pc.signalingState}")
+
     async with websockets.connect(SERVER_URL) as websocket:
         await websocket.send(json.dumps({"type": "register", "user_id": "answerer"}))
         response = await websocket.recv()
+        print(f'Response received: {response}')
         # print('3')
         data = json.loads(response)
         # print(data)
@@ -113,6 +169,7 @@ async def run_answer():
         if data["type"] == "offer":
             offer = RTCSessionDescription(sdp=data["sdp"], type="offer")
             await pc.setRemoteDescription(offer)
+            print('remote description set')
             # print('5')
 
             answer = await pc.createAnswer()
@@ -120,13 +177,15 @@ async def run_answer():
             await pc.setLocalDescription(answer)
             # print('7')
 
-            await websocket.send(json.dumps({"type": "answer", "sdp": answer.sdp, "user_id": "answerer"}))
+            await websocket.send(json.dumps({"type": "answer", "sdp": pc.localDescription.sdp, "user_id": "answerer"}))
+            print('Answer sent')
             # print('8')
 
     try:
         await asyncio.Future()
     finally:
         await pc.close()
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] not in ("offer", "answer"):
