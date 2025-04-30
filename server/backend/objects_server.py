c0a762700350e19949e51639073b77ae5d508b8f
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2025-04-30 23:46:26 +0300

    User validation during chat creation was fixed

--- server/backend/objects_server.py
+++ server/backend/objects_server.py
@@ -506,18 +506,18 @@ class Server:
 
     async def __handle_user_existance_request(self, websocket, user_id: str, data: dict):
         """Checks if user are registred on the server"""
-
+        target_user_id = data["target_user_id"]
         try:
             conn = await asyncpg.connect(self.SERVER_DATABASE_URL)
 
             row = await conn.fetchval(
                 "SELECT EXISTS(SELECT 1 FROM users WHERE user_id = $1)",
-                user_id
+                target_user_id
             )
 
             user_existance_request = Request(
                 request_type="check_user_existance_request",
-                content={"user_id": user_id, "user_existance": bool(row)}
+                content={"target_user_id": target_user_id, "user_existance": bool(row)}
             )
             await websocket.send(user_existance_request.json_string)
         finally:
